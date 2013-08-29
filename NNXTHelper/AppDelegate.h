0891f1818414d067314e187fe0a8074c057ca8c7
Morgan <gormster@me.com>
2013-08-29 12:34:49 +1000

    initial commit

--- NNXTHelper/AppDelegate.h
+++ NNXTHelper/AppDelegate.h
@@ -12,4 +12,21 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+@property double beatsLong;
+@property double beatsLongMultiplier;
+@property double startPosition;
+@property double startPositionMultiplier;
+@property double endPosition;
+@property double endPositionMultiplier;
+@property NSUInteger framesLong;
+
+@property (weak) IBOutlet NSTextField *sampleLengthBeatsBarsLabel;
+
+@property (weak) IBOutlet NSTextField *startPositionLabel;
+@property (weak) IBOutlet NSTextField *endPositionLabel;
+
+- (IBAction)changeSampleLengthMultiplier:(NSPopUpButton *)sender;
+- (IBAction)changeStartMultiplier:(NSPopUpButton *)sender;
+- (IBAction)changeEndMultiplier:(NSPopUpButton *)sender;
+
 @end
